2d29eaa11d658a840d1da1fc590bafb8ab08900f
Ferszus <hubparylak2@gmail.com>
2022-05-17 21:32:55 +0200

    quick fix: All Inputs musn't be empty

--- script.php
+++ script.php
@@ -1,6 +1,6 @@
 <?php
 if (isset($_POST["oblicz"])) {
-    if (empty($_POST["a"]) && empty($_POST["b"]) && empty($_POST["c"])) {
+    if (empty($_POST["a"]) || empty($_POST["b"]) || empty($_POST["c"])) {
         echo "Wypełnij wszystkie pola";
     } elseif ($_POST["a"] >= 1) {
         require "./greater_than_0.php";
