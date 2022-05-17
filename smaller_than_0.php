b69302dc446670740a30349297fef9dd1fa92d3c
Ferszus <hubparylak2@gmail.com>
2022-05-17 21:40:34 +0200

    fix: x1, x2 switch

--- smaller_than_0.php
+++ smaller_than_0.php
@@ -23,7 +23,7 @@ echo "<p class='info'>f(x) = " . $a . "x<sup style='font-size: 1rem;'>2</sup> + 
 if ($delta > 0) {
     $x1 = round(($b * -1 - round(sqrt($delta), 2)) / (2 * $a), 2);
     $x2 = round(($b * -1 + round(sqrt($delta), 2)) / (2 * $a), 2);
-    echo "x1 = $x1, x2 = $x2";
+    echo "x1 = $x2, x2 = $x1";
 } elseif ($delta == 0) {
     $x1 = ($b * -1) / (2 * $a);
     echo "x1 = $x1";
